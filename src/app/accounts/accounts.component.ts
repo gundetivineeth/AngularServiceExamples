ed8b29a097a199ff1c5f319330f81c43f681a67e
gundetivineeth <vineethg508@gmail.com>
2022-01-17 07:54:13 -0700

    Logging service angular example

--- src/app/accounts/accounts.component.ts
+++ src/app/accounts/accounts.component.ts
@@ -1,26 +1,31 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-accounts',
   templateUrl: './accounts.component.html',
-  styleUrls: ['./accounts.component.css']
+  styleUrls: ['./accounts.component.css'],
+  providers:[LoggingService]
 })
 export class AccountsComponent implements OnInit {
 
   @Input() accounts;
 
-  constructor() { }
+  constructor(private loggingService:LoggingService) { }
 
   ngOnInit(): void {
   }
 
   onClickActive(){
     this.accounts.status = "Active";
+    this.loggingService.logToConsole("Active");
   }
   onClickInActive(){
     this.accounts.status = "InActive";
+    this.loggingService.logToConsole("InActive");
   }
   onClickUnknown(){
     this.accounts.status = "Unknown";
+    this.loggingService.logToConsole("Unknown");
   }
 }
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,16 +1,19 @@
 import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers:[LoggingService]
 })
 export class NewAccountComponent implements OnInit {
 
   @Output() accounts = new EventEmitter<{accountName:string,status:string}>();
-  constructor() { }
   @ViewChild('accountNameRef') accountNameRef:ElementRef;
   @ViewChild('statusRef') statusRef:ElementRef;
+
+  constructor(private loggingService:LoggingService) { }
 
   ngOnInit(): void {
   }
@@ -19,7 +22,8 @@ export class NewAccountComponent implements OnInit {
     this.accounts.emit({
       accountName:this.accountNameRef.nativeElement.value,
       status:this.statusRef.nativeElement.value
-    })
+    });
+    this.loggingService.logToConsole(this.statusRef.nativeElement.value);
   }
 
 }
